46a9f26adc73ba22f8691324dde7903a2a9ea01e
Phil <langphi>
2014-11-02 20:06:23 +0000

    First load

---
+++ ellipse.js
@@ -0,0 +1,97 @@
+$(document).ready(function () {
+  var marquee = function ($display, showOpts, hideOpts, loop) {
+    if (loop > 0) {
+      hideOpts.complete = marquee($display, showOpts, hideOpts, loop - 1);
+      return function () {
+        console.log('showing & hiding');
+        console.log($display);
+        $display
+          .show(showOpts)
+          .hide(hideOpts);
+      };
+    }
+  };
+  $('.display').each(function () {
+    var 
+      $this = $(this),
+      $next = $this.next('.display').eq(0),
+      showAt = $this.attr('data-display-at') * 40,
+      hideAt = $next.attr('data-display-at') * 40,
+      duration = ($this.attr('data-transition') || 0) * 40,
+      nextDuration = $next.attr('data-transition') * 40,
+      easing = ($this.hasClass('right') || $this.hasClass('left')) ? 'linear' : 'swing',
+      nextEasing = ($next.hasClass('right') || $next.hasClass('left')) ? 'linear' : 'swing';
+    if ($this.hasClass('up') || $this.hasClass('down') || $this.hasClass('downup')) {
+      effect = 'slide';
+    } else if ($this.hasClass('right')) {
+      effect = 'slide';
+    } else if ($this.hasClass('left')) {
+      effect = 'blind';
+    } else {
+      effect = 'fade';
+    }
+    if ($next.hasClass('up') || $next.hasClass('down') || $next.hasClass('downup')) {
+      nextEffect = 'slide';
+    } else if ($next.hasClass('right')) {
+      nextEffect = 'slide';
+    } else if ($next.hasClass('left')) {
+      nextEffect = 'blind';
+    } else {
+      nextEffect = 'fade';
+    }
+    if ($this.hasClass('up') || $this.hasClass('downup')) {
+      direction = 'down';
+    } else if ($this.hasClass('down')) {
+      direction = 'up';
+    } else if ($this.hasClass('left')) {
+      direction = 'left';
+    } else if ($this.hasClass('right')) {
+      direction = 'right';
+    }
+    if ($next.hasClass('up')) {
+      nextDirection = 'up';
+    } else if ($next.hasClass('down') || $next.hasClass('downup')) {
+      nextDirection = 'down';
+    } else if ($next.hasClass('left')) {
+      nextDirection = 'right';
+    } else {
+      nextDirection = 'left';
+    }
+    showOpts = {
+      effect: effect,
+      duration: duration,
+      direction: direction,
+      easing: easing
+    };
+    hideOpts = {
+      effect: nextEffect,
+      duration: nextDuration,
+      direction: nextDirection,
+      easing: nextEasing
+    };
+    $this
+      .delay(showAt)
+      .show(showOpts);
+    if ($next.length > 0) {
+      if ($next.hasClass('downup')) {
+        $this
+          .delay(hideAt - showAt - duration - nextDuration)
+          .hide(hideOpts);
+      } else if (nextEffect === 'slide' || nextEffect === 'blind') {
+        if (nextDirection === 'right' || nextDirection === 'left') {
+          console.log('delaying hiding for ' + (hideAt - showAt - duration + nextDuration));
+          $this
+            .delay((hideAt - showAt - duration) + nextDuration)
+            .hide({
+              effect: 'fade',
+              duration: 0
+            });
+        } else {
+          $this
+            .delay(hideAt - showAt - duration)
+            .hide(hideOpts);
+        }
+      }
+    }
+  });
+});
